cc12d12b50bd236124fce6a413572ff2a13ecfff
Aaron <wisdafu@att.net>
2017-03-04 01:11:47 -0600

    Dunno but done for tonight

--- game/src/panelUI.java
+++ game/src/panelUI.java
@@ -8,12 +8,22 @@ import java.awt.event.*;
 public class panelUI {
     private JFrame mainFrame;
     private JLabel messageLabel;
+    private JTextField inputPanel;
 
     public panelUI(){
         mainFrame = new JFrame("CPG");
-        messageLabel = new JLabel("HELLO!",JLabel.CENTER);
+        //mainFrame.setLayout(new BorderLayout());
+        messageLabel = new JLabel("HELLO!");
+        inputPanel = new JTextField("> ");
+        messageLabel.setLayout(new BorderLayout());
+        inputPanel.setLayout(new BorderLayout());
         mainFrame.setSize(600,400);
-        mainFrame.add(messageLabel);
+        mainFrame.add(messageLabel, BorderLayout.CENTER);
+        mainFrame.add(inputPanel, BorderLayout.SOUTH);
         mainFrame.setVisible(true);
+    }
+
+    public void displayMessage(commandLine commandLineInst){
+
     }
 }
