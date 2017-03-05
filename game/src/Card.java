32a789b3223a95e0d770fad30455ae5238e49a7b
Drackaris <stevestranczek@gmail.com>
2017-03-05 12:16:33 -0600

    Add files via upload

---
+++ game/src/Card.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Steve on 3/5/2017.
+ */
+public abstract class Card {
+    private String text;
+    private int cooldown;
+    private int tempCD;
+
+    Card(String cardText, int cd){
+        text = cardText;
+        cooldown = cd;
+        tempCD = 0;
+    }
+
+    public boolean checkCooldown(){
+        if(tempCD == 0)
+        {
+            return true;
+        }
+        else
+        {
+            int timeleft = cooldown - tempCD;
+            System.out.println("card is on cooldown for "+timeleft+"turns");
+            return false;
+        }
+    }
+
+    public void incrementCooldown(){
+        if(tempCD != 0)
+          tempCD = tempCD++;
+        else if (tempCD < cooldown){
+            tempCD = tempCD++;
+        }
+        else {
+            tempCD = 0;
+        }
+    }
+
+    public void cardFunction(Player p, Player e) {
+        if (checkCooldown()) {
+        }
+        else {
+            System.out.println("Card is on cooldown!");
+        }
+    }
+}
