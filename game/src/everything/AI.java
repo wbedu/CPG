ee876043fde102e20afe3176c77812e1ed9949b0
Wilfried Christophe Bedu <cbedu2@uic.edu>
2017-03-10 19:14:52 -0600

    begin work on AI.java

---
+++ game/src/everything/AI.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+
+public class AI{
+    private Player enemy;
+    private 
+    private AI(){
+
+    }
+
+    public AI(Player newEnemy){
+        enemy= newEnemy;
+    }
+
+
+}
