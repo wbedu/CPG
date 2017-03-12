7004fb7d17546d3141da77439aa3e3df8d3d7861
Christophe Bedu <cbedu2@uic.edu>
2017-03-12 19:01:27 -0400

    set TODO flags for pasing AI card usage

--- game/src/everything/AI.java
+++ game/src/everything/AI.java
@@ -2,15 +2,62 @@ package everything;
 import java.util.*;
 
 
-public class AI{
-    private Player enemy; 
-    private AI(){
+public abstract class AI extends Player{
+	
+	protected int healthTreshold;
+	
+	
+	protected AI(){
+		super();
+	}
 
-    }
-
-    public AI(Player newEnemy){
-        enemy= newEnemy;
-    }
-
-
+	protected AI(String newName,int newHealth,int newAttackPoints,int newdefensePoints, Deck newHand,int newHealthTreshold){
+		super(newName,newHealth,newAttackPoints,newdefensePoints,newHand);
+		healthTreshold=this.health;
+		
+	}
+	
+	
+	void setNextCard(Player user){
+		
+		boolean moveset=false;
+		if(isHealthy()){
+			//TODO
+			//check if heal card available
+			//set next card to heal
+			//check if escape card available
+			//set next to to escape
+			moveset=true;
+		}
+		
+		if((moveset==false)&&(user.defensePoints>this.attackPoints)){
+			//TODO
+			//check if buff card available;
+			//if buff card available set next to attack
+			moveset=true;
+		}
+		
+		if(moveset==false){
+			
+			//attempt to roll for special card
+			//set next cart to special card
+			//else
+			//set next card to random attack
+		}
+		
+		
+		
+		
+	}
+	
+	//call function or cards available that can repleanish health;
+	protected boolean isHealthy(){
+	
+		if(this.health>healthTreshold){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
 }
