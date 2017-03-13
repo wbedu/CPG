6eecdeddc60d6dbfb0dfa466788b9be2e11a3c3c
Aaron <wisdafu@att.net>
2017-03-13 18:40:00 -0500

    added saveState skeleton

---
+++ game/src/everything/SaveState.java
@@ -0,0 +1,19 @@
+package everything;
+
+/**
+ * Created by Aaron on 3/13/17.
+ */
+public class SaveState {
+    String saveFile = "savePath/saveFile.json";
+
+    private void saveGame(){
+        System.out.println("game has been saved");
+    }
+
+    private void loadGame(){
+        System.out.println("game has been loaded");
+
+    }
+
+
+}
