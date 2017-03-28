914a829c74b197c749f5bcee111ba09171a1ed76
Aaron <wisdafu@att.net>
2017-03-27 23:20:58 -0500

    implemented card number updater

--- game/src/everything/client.java
+++ game/src/everything/client.java
@@ -7,10 +7,13 @@ public class client {
     public static void main(String args[]){
         boolean gameLoop = true;
         panelUI panelUIInst = new panelUI();
+        int cardSelected;
 
         panelUIInst.setMessage("\nBattle with 'Shrak' the Ogre has begun!\n");    //this function takes a String and puts it in the message display
 
         while(gameLoop){
+            cardSelected = panelUIInst.getCardSelect(); //This gets the current selected card
+                                                        //Pass into Battle
 
         }
     }
