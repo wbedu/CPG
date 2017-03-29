c1330a26920b8328d5a765126b2f7256c0f72e3a
Christophe Bedu <cbedu2@uic.edu>
2017-03-29 00:17:05 -0500

    #

--- game/src/everything/Hand.java
+++ game/src/everything/Hand.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 
 public class Hand{
 	private static int maxCards=5;
-	private int handSize;
 	protected ArrayList<Card> hand;
 	protected ArrayList<Card> deck;	
 
@@ -24,7 +23,7 @@ public class Hand{
 	
     protected void addCard(Card newCard){
     	
-    	if(handSize>=maxCards){
+    	if(hand.size()>=maxCards){
     		
     		System.out.println("Error: maximum number of cards reached");
     		System.out.println("Abort addCard");
@@ -39,6 +38,8 @@ public class Hand{
     		
     		System.out.println("card add");
     	//}
+    		
+    
     }
     
     protected void removeCard(Card removedCard){
