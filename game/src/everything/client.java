d59e6868403da44b3a69b2e56b22030a05fe26f6
Aaron <wisdafu@att.net>
2017-03-28 22:15:54 -0500

    implemenation of hand

--- game/src/everything/client.java
+++ game/src/everything/client.java
@@ -1,4 +1,5 @@
 package everything;
+import everything.cardPackage.*;
 
 /**
  * Created by Aaron on 3/4/17.
@@ -7,11 +8,38 @@ public class client {
     public static void main(String args[]){
         boolean gameLoop = true;
         panelUI panelUIInst = new panelUI();
-        int cardSelected;
+        Battle testBattle;
+        User userPlayer;
+        AI enemyAI;
+        int cardSelected, needBattle = 1;
+
+        //Hand setup
+        Block blockCard = new Block();
+        Cleave cleaveCard = new Cleave();
+        Mutton muttonCard = new Mutton();
+        NordicBlood nordCard = new NordicBlood();
+        SavageStrike savageCard = new SavageStrike();
+
+        Hand playerHand = new Hand();
+        playerHand.addCard(blockCard);
+        playerHand.addCard(cleaveCard);
+        playerHand.addCard(muttonCard);
+        playerHand.addCard(nordCard);
+        playerHand.addCard(savageCard);
+
+        Deck enemyDeck = new Deck();
 
         panelUIInst.setMessage("\nBattle with 'Shrak' the Ogre has begun!\n");    //this function takes a String and puts it in the message display
 
+        userPlayer = new User("Aaron",10,3,3,playerHand);
+        enemyAI = new AI("Shrak",10,1,2,enemyDeck,3);
+
+        //First Battle
         while(gameLoop){
+            if(needBattle==1){
+                testBattle = new Battle(userPlayer, enemyAI);
+                needBattle = 0;
+            }
             while(panelUIInst.getCardWaiting()){
 
             }
@@ -20,5 +48,8 @@ public class client {
             //DO BATTLE
             panelUIInst.setCardWaitingTrue();
         }
+
+        //End battle if statement here
     }
+
 }
