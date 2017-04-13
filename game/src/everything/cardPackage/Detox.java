01f311e8ab0a33d96b776eb997adf1d5e95f4e54
Christophe Bedu <cbedu2@uic.edu>
2017-04-13 01:06:20 -0500

    Detox card - clears all player status affects

--- game/src/everything/cardPackage/Detox.java
+++ game/src/everything/cardPackage/Detox.java
@@ -5,8 +5,6 @@ public class Detox extends Card{
     public Detox(){super("Clears all status effects from player","Detox", "Utility", 5); tempCD = 0;}
 
     public void cardFunction(Player p1, Player p2){
-    	/*for(int i=0; i<p1.status.size(),i++){
-    		
-    	}*/
+    	p1.statusManager.clearAllStatus();
     }
 }
