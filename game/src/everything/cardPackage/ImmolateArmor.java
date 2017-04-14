81246da617be82db207c34881782e19515865dd5
Steve <sstran2@uic.edu>
2017-04-14 17:06:36 -0500

    Added immolate armor card.

---
+++ game/src/everything/cardPackage/ImmolateArmor.java
@@ -0,0 +1,17 @@
+package everything.cardPackage;
+
+import everything.Player;
+/**
+ * Created by Steve on 4/14/2017.
+ */
+public class ImmolateArmor extends Card {
+    public ImmolateArmor() {
+        super("Melt the opponents armor, dealing damage equal to it", "Immolate Armor", "Utility", 3, 0); tempCD = 0;
+    }
+
+    public void cardFunction(Player p1, Player p2){
+        int dmgVal = p2.getDefense();
+        p2.decreaseDefense(dmgVal);
+        p2.decreaseHealth(dmgVal);
+    }
+}
