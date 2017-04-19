86c01abec2b6ab657700c0c327d5b7f4e94416e1
Christophe Bedu <cbedu2@uic.edu>
2017-04-18 20:25:48 -0500

    new Player constructed takes prebuilt Hand

--- game/src/everything/Player.java
+++ game/src/everything/Player.java
@@ -34,6 +34,11 @@ public abstract class Player {
         level=0;
         money=0;
     }
+    
+    public Player(String name, int health,int defensePoints, Hand hand){
+    	this(name,health,defensePoints);
+    	this.hand = hand;
+    }
 
     //decreases health but does not change any other stats
     public void decreaseHealth(int decreaseFactor){
