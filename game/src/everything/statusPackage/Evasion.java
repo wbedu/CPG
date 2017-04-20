b45c256565c11fa5b80a5cb13d93f713d2708915
Steve <stevestranczek@gmail.com>
2017-04-19 22:04:56 -0500

    added evasion status

---
+++ game/src/everything/statusPackage/Evasion.java
@@ -0,0 +1,32 @@
+package everything.statusPackage;
+
+import everything.Player;
+
+/**
+ * Created by Steve on 4/19/2017.
+ */
+public class Evasion extends Status {
+    int EvasionPercent;
+    int health;
+
+    public Evasion(Player newUser, int numTurns, int EvadeChance) {
+        user = newUser;
+        turns = numTurns;
+        EvasionPercent = EvadeChance;
+        health = newUser.getHealth();
+    }
+
+    @Override
+    public void statusEffect() {
+        turns--;
+        if (user.getHealth() < health) {
+            int randVal = (int)(Math.random() * 100);
+            if(randVal <= EvasionPercent){
+                user.increaseHealth(health - user.getHealth());
+            }
+        }
+        else{
+            health = user.getHealth();
+        }
+    }
+}
