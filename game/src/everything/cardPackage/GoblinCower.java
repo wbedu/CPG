bab12013ed73efa68a23c514365866f150484df2
Steve <stevestranzek@gmail.com>
2017-04-21 19:45:10 -0600

    Fixed GoblinCower

--- game/src/everything/cardPackage/GoblinCower.java
+++ game/src/everything/cardPackage/GoblinCower.java
@@ -1,7 +1,18 @@
 package everything.cardPackage;
 
+import everything.Player;
+
 /**
  * Created by Steve on 4/17/2017.
  */
 public class GoblinCower extends Card {
+
+    public GoblinCower() {
+        super("Goblin cowers and does nothing","Cower", "Utility", 0,0, false);
+
+    }
+
+    @Override
+    public void cardFunction(Player p1, Player p2) {
+    }
 }
