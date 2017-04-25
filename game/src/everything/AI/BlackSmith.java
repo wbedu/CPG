4ead712f7d4e6530d1cd78ebbe7001e8a680a723
Christophe Bedu <cbedu2@uic.edu>
2017-04-24 21:31:50 -0500

    first boss move patterns

--- game/src/everything/AI/BlackSmith.java
+++ game/src/everything/AI/BlackSmith.java
@@ -8,16 +8,52 @@ import everything.Player;
 public class BlackSmith extends AI{
         public BlackSmith(Player user){
             super("Inawa",50,0, user);
-            this.addCard(new BSArmorUp());
-            this.addCard(new BSBasicAttack());
-            this.addCard(new BSBerserk());
-            this.addCard(new BSHammerToss());
-            this.addCard(new BSKeenEdge());
-            this.addCard(new BSMoltenWeapon());
+            this.addCard(new BSArmorUp());//card0
+            this.addCard(new BSBasicAttack());//card1
+            this.addCard(new BSBerserk());//card2
+            this.addCard(new BSHammerToss());//card3
+            this.addCard(new BSKeenEdge());//card4
+            this.addCard(new BSMoltenWeapon());//card5
             depositMoney(50);
         }
         @Override
         public void setNextCard(){
-            nextCard =randomWithRange(0,getHandSize()-1);
+        	int rand = (int)(Math.random()*100);
+        	if(getDefense()==0||(rand<20)){
+        		nextCard=0;
+        		return;
+        	}
+        	
+        	if(enemy.getHealth()>this.health){
+        		
+        		if(this.getDefense()>(enemy.getHealth()/2)){
+        			nextCard=2;
+        		}
+        		nextCard=5;
+        		return;
+        	}
+        	
+        	
+        	if(rand+getDefense()>70){
+        		nextCard=2;
+        		return;
+       		}
+        		
+        	if(rand>60){
+        		nextCard=3;
+       			return;
+       		}
+       		
+       		if(rand>50){
+       			nextCard=4;
+       			return;
+       		}
+       		
+        	if(rand>40){
+        		nextCard=5;
+        		return;
+        	}
+        		
+            nextCard =1;
         }
 }
